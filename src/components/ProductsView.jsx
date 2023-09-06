09dd5a2f92585df0fd9f88ac213027d081b441d2
ROGUEANOVI <ovidioromero66@gmail.com>
2023-09-06 10:42:24 -0500

    getting data from backend

--- src/components/ProductsView.jsx
+++ src/components/ProductsView.jsx
@@ -4,12 +4,21 @@ import { ProductItemView } from './ProductItemView';
 
 export const ProductsView = ({ handlerAddProductToCart }) => {
   const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    setProducts(getProducts());
+    getData();
   }, []);
+
+  const getData = async () => {
+    const data = await getProducts();
+    setProducts(data);
+    setIsLoading(false);
+  };
+
   return (
     <>
+      {isLoading && <div className='alert alert-info'>Cargando...</div>}
       <div className='row'>
         {products.map(product => (
           <div className='col-4 my-2' key={product.id}>
